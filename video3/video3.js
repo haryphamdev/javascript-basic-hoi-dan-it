4efa7804ef74842edc2aa2d92c96e30b8b724972
Hary Pham <haryphamdev@gmail.com>
2021-08-21 19:27:34 +0700

    chapter 2 video 3

---
+++ video3/video3.js
@@ -0,0 +1,14 @@
+
+// const x = 5;
+// x = 6;
+console.log(`Hello' '" ' world from javascript: `)
+
+let x = 5, y = 6, z = 7;
+
+console.log(" Test string", 'x= ', x, ' y= ', y, ' z=', z)
+
+console.log(" Test string 2222 :", + 'x = ' + x + 'y= ' + y + 'z= ' + z)
+
+
+
+
