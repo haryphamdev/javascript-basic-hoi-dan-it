b48a9edd9859e9738f22364775eaf7dfa891594f
Hary Pham <haryphamdev@gmail.com>
2021-08-22 16:59:27 +0700

    chapter 2 video 6

---
+++ video6/video6.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML'.toUpperCase())
+let a = 5; //int, float, long, double => only number
+let b = 6;
+let c = 6.9
+let d = '5'
+let e = +d;
+let f = Number(d)
+console.log('e value: ', e, 'type: ', typeof e)
+console.log(a === e, 'a: ', typeof a, 'd: ', typeof d)
