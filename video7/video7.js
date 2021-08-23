65e82367b3e06ed494301c077717d61fc34c0d39
Hary Pham <haryphamdev@gmail.com>
2021-08-23 20:21:40 +0700

    chapter 2 video 7

---
+++ video7/video7.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML')
+let a = 5;
+let b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+console.log('Fifteen is ' + a + b + ' and not ' + (2 * a + b) + '.');
+
+// url: '/hoidanit/5
+console.log(`Hoi dan it check strings: a = ${a} b= ${b} a+b = ${a + b}`)
+console.log(`/hoidanit/${a}`)
