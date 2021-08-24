e4d01f1ace21355808919e65bb7002a36c06366d
Hary Pham <haryphamdev@gmail.com>
2021-08-24 20:05:20 +0700

    chapter 2 video 8

---
+++ video8/video8.js
@@ -0,0 +1,21 @@
+console.log('Hello world from HTML');
+
+//key: value
+let obj = {
+    name: 'Eric',
+    address: 'Ha Noi',
+    email: '',
+    gender: ''
+};
+
+
+
+
+let b = 'address';
+
+// obj[b] = 'Hary'
+obj.name = 'HCM'
+
+console.log('what is your name ? ', `My name 's : `, obj['name'], obj)
+console.log('Where are you from ? ', `I'm from : `, obj['address'])
+
