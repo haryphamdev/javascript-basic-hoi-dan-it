4fbfa24f589017bdf0a35996ea02533de472b8f2
Hary Pham <haryphamdev@gmail.com>
2021-08-24 20:30:25 +0700

    chapter 2 video 9

---
+++ video9/video9.js
@@ -0,0 +1,19 @@
+console.log('Hello world from HTML');
+let a = {
+    name: 'Eric',
+    age: 25
+
+}; //key:value
+
+console.log('check data object: ', a)
+console.log('my name\'s : ', a.name)
+
+//arr = [item1, item2, item3]
+let b = ['MU', 'Chelsea', "Liver"];
+console.log('typof b: ', typeof b, b)
+console.log('first item: ', b[0], ' second item: ', b[1])
+let family = [
+    { name: 'Eric', age: 25 },
+    { name: 'Monica', age: 24 }
+]
+console.log('my family: ', family)
