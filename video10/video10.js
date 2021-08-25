bb8d74e2c6e0203632c5e6a9b4dd736302e4e9ae
Hary Pham <haryphamdev@gmail.com>
2021-08-25 16:38:43 +0700

    chapter 2 video 10

---
+++ video10/video10.js
@@ -0,0 +1,21 @@
+console.log('Hello world from HTML')
+let a = 5; // x <- y
+let b = '5';
+
+console.log(' compare a == b: ', 'a = ', a, ' b = ', a, ' result: ', a == b)
+console.log(' compare a === b: ', 'a = ', a, ' b = ', a, ' result: ', a === b)
+// === type, value convention
+
+// Empty, null, undefined
+let c = null;
+console.log('check value: ', c)
+let d = '';
+d = 5;
+d = ['11']
+
+
+
+
+
+
+
