9f9c5b44ec1f3d9316120e523d8fbb94754c02ed
Hary Pham <haryphamdev@gmail.com>
2021-08-25 20:28:20 +0700

    chapter 3 video 1

---
+++ video11/video11.js
@@ -0,0 +1,14 @@
+console.log('Hello world from HTML')
+
+let arr = ['Mu', 'ManCity', 'Liver', 'Chelsea']
+
+let i = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    console.log('check variable i = ', arr[i],)
+}
+
+//
+console.log('check value i: ', i)
+
+
