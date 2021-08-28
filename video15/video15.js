27b664e8edf28318737083eb624c9c7d40c9b079
Hary Pham <haryphamdev@gmail.com>
2021-08-28 14:53:38 +0700

    chapter 3 video 5

---
+++ video15/video15.js
@@ -0,0 +1,11 @@
+console.log('Hello world from HTML')
+//global
+//block
+
+// console.log('value x: ', x)
+// let x = 5;
+
+
+for (let i = 0; i < 10; i++) {
+    console.log('>>> check value i: ', i)
+}
