1128deff80faf3ac29212b89d25b8cfae5e96b0e
Hary Pham <haryphamdev@gmail.com>
2021-08-29 10:21:17 +0700

    chapter 4 video3

---
+++ video18/video18.js
@@ -0,0 +1,13 @@
+console.log('Hello world from HTML')
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+
+//arrow function 
+let sum = (a, b) => {
+    return a + b;
+}
+
+console.log(' check sum: ', sum(6, 9))
